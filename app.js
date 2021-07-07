8bb66c78d7af0f340b136d7dc9e58609fe33ed38
wilson614 <s110613053@stu.ntue.edu.tw>
2021-07-08 02:09:41 +0800

    Add detail page

--- app.js
+++ app.js
@@ -40,6 +40,14 @@ app.post('/todos', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  Todo.findById(id)
+    .lean()
+    .then(todo => res.render('detail', { todo }))
+    .catch(error => console.log(error))
+})
+
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
 })
