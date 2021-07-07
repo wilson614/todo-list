664beb85634df24946feb70d67f4bca61cd0510d
wilson614 <s110613053@stu.ntue.edu.tw>
2021-07-08 02:51:57 +0800

    Add delete todo function

--- app.js
+++ app.js
@@ -68,6 +68,14 @@ app.post('/todos/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  Todo.findById(id)
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
 })
