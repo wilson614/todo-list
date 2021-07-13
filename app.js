88622a02fad820b6487ffff05da641fccc9ed46f
wilson614 <s110613053@stu.ntue.edu.tw>
2021-07-14 02:22:33 +0800

    Modify mongoose config

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 const port = 3000
@@ -12,18 +13,6 @@ app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
-
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,17 +1,7 @@
-const mongoose = require('mongoose')
 const Todo = require('../todo')
+const db = require('../../config/mongoose')
 
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-
+db.once('open', () => {  
   for (let i = 0; i < 10; i++) {
     Todo.create({ name: `name-${i}`})
   }
