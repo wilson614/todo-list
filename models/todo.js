b52db09fb1c908f892e3a0a5d42e05db8b093a65
wilson614 <s110613053@stu.ntue.edu.tw>
2021-07-13 23:46:54 +0800

    Add isDone params

--- models/todo.js
+++ models/todo.js
@@ -3,7 +3,11 @@ const Schema = mongoose.Schema
 const todoSchema = new Schema({
   name: {
     type: String,
-    required: true,
+    required: true
+  },
+  isDone: {
+    type: Boolean,
+    default: false
   }
 })
 module.exports = mongoose.model('Todo', todoSchema)
