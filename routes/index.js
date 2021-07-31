32a76824e468515f2a6ea4f60b2cff71f060ec0c
wilson614 <s110613053@stu.ntue.edu.tw>
2021-07-31 15:44:52 +0800

    Add user modules

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const home = require('./module/home')
 const todo = require('./module/todos')
+const users = require('./module/users')
 
 router.use('/', home)
 router.use('/todos', todo)
+router.use('/users', users)
 
 module.exports = router
