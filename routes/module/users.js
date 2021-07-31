a3839cab0dcce437c46eb39356a20d49fdb44451
wilson614 <s110613053@stu.ntue.edu.tw>
2021-07-31 17:03:22 +0800

    Add user registration function

--- routes/module/users.js
+++ routes/module/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -10,6 +11,26 @@ router.post('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('Email already exists!')
+        res.render('register', { name, email, password, confirmPassword })
+      } else {
+        User.create({
+          name,
+          email,
+          password
+        })
+          .then(() => res.redirect('/'))
+          .catch(error => console.log(error))
+      }
+    })
+    .catch(error => console.log(error))
 })
 
 module.exports = router
