8d5cddb295a88df0461b585028ed3ccb6bef0c1c
wilson614 <s110613053@stu.ntue.edu.tw>
2021-07-31 22:57:20 +0800

    Add message partials

--- routes/module/users.js
+++ routes/module/users.js
@@ -18,26 +18,37 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '每個欄位都是必填！' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '兩次輸入的密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', { name, email, password, confirmPassword, errors })
+  }
+
   User.findOne({ email })
     .then(user => {
       if (user) {
-        console.log('Email already exists!')
-        res.render('register', { name, email, password, confirmPassword })
-      } else {
-        User.create({
-          name,
-          email,
-          password
-        })
-          .then(() => res.redirect('/'))
-          .catch(error => console.log(error))
+        errors.push({ message: '這個信箱已經註冊過了！' })
+        return res.render('register', { name, email, password, confirmPassword, errors })
       }
+      User.create({
+        name,
+        email,
+        password
+      })
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
     })
     .catch(error => console.log(error))
 })
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出！')
   res.redirect('/users/login')
 })
 
